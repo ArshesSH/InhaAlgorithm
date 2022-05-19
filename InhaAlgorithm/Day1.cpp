acdd106b4bafb288ef8dc2fb988395b068ed7873
ArshesSH <53480645+ArshesSH@users.noreply.github.com>
2022-05-19 14:46:42 +0900

    update

--- InhaAlgorithm/Day1.cpp
+++ InhaAlgorithm/Day1.cpp
@@ -13,43 +13,40 @@ int Day1::max4( int a, int b, int c, int d )
 
 int Day1::med3( int a, int b, int c )
 {
-    int mid;
-    if ( a < b )
-    {
-        if ( b < c )
-        {
-            mid = b;
-        }
-        else
-        {
-            mid = c;
-        }
-    }
-    else if( b < c )
-    {
-        mid = b;
-    }
-    else if (a < c )
-    {
-        mid = c;
-    }
-    else
-    {
-        mid = a;
-    }
+	int mid;
 
-    return mid;
+	if ( a < b )
+	{
+		if ( b < c )
+		{
+			mid = b;
+		}
+		else if ( a < c )
+		{
+			mid = c;
+		}
+		else
+		{
+			mid = a;
+		}
+	}
+	else if ( a < c )
+	{
+		mid = a;
+	}
+	else if ( b < c )
+	{
+		mid = c;
+	}
+	else
+	{
+		mid = b;
+	}
+	return mid;
 }
 
 int Day1::GetMax( int lhs, int rhs )
 {
     return (lhs > rhs) ? lhs : rhs;
-}
-
-void Day1::Swap( int& lhs, int& rhs )
-{
-    int tmp = lhs;
-    lhs = rhs;
-    rhs = lhs;
 }
 
