dba366e7ffacbcf6930a7ad1ed2bbfcb45c29d39
ArshesSH <53480645+ArshesSH@users.noreply.github.com>
2022-06-29 16:49:23 +0900

    Update Substrkmp

--- InhaAlgorithm/Day19.cpp
+++ InhaAlgorithm/Day19.cpp
@@ -1,11 +1,28 @@
 #include "Day19.h"
 
-#include "StringSercher.h"
+#include "StopWatch.h"
+#include "StringSearcher.h"
 
 void Day19::ExSearchString()
 {
-	const std::string str1 = "ABABCDEFGHA";
-	const std::string str2 = "ABC";
+	const std::string str1 = "ABABDJNABABBEABGHABCABDAB";
+	const std::string str2 = "ABCABD";
 
-	FindSubStr_Brute( str1.c_str(), str2.c_str(), true );
+	StopWatch timer;
+	
+	timer.Start();
+	FindSubStr_Brute( str1.c_str(), str2.c_str(), false );
+	timer.Stop();
+
+	const double elapsed1 = timer.GetElapsedTime<double, (int)StopWatch::TimeUnit::Milli>();
+	std::cout << "걸린 시간 : " << elapsed1 << std::endl;
+
+
+	timer.Start();
+	//FindSubStr_KMP( str1.c_str(), str2.c_str(), str2.size(), false );
+	timer.Stop();
+
+	FindSubStr_KMP( str1.c_str(), str2.c_str(), str2.size(), true );
+	const double elapsed2 = timer.GetElapsedTime<double, (int)StopWatch::TimeUnit::Milli>();
+	std::cout << "걸린 시간 : " << elapsed1 << std::endl;
 }
---
+++ InhaAlgorithm/StringSearcher.h
@@ -0,0 +1,168 @@
+#pragma once
+
+#include <iostream>
+
+void PrintStrSearch( const char* srcStr, const char* targetStr, int srcCursor, int searchCur, int targetCursor)
+{
+	printf( "\t%s\n", srcStr );
+	printf( "\t" );
+	for ( int i = 0; i < srcCursor + targetCursor; ++i )
+	{
+		printf( " " );
+	}
+	if ( srcStr[searchCur] == targetStr[targetCursor] )
+	{
+		printf( "+\n" );
+	}
+	else
+	{
+		printf( "|\n" );
+	}
+	printf( "\t" );
+	for ( int i = 0; i < srcCursor; ++i )
+	{
+		printf( " " );
+	}
+	printf("%s\n\n", targetStr );
+}
+
+int FindSubStr_Brute(const char* srcStr, const char* targetStr, bool isPrintProcess)
+{
+	int idxSrc = 0;
+	int count = 0;
+	for ( ; srcStr[idxSrc] != '\0'; ++idxSrc )
+	{
+		int idxTarget = 0;
+		for ( int idxSearchStart = idxSrc ; targetStr[idxTarget] != '\0'; ++idxTarget, ++idxSearchStart )
+		{
+			if ( isPrintProcess )
+			{
+				++count;
+				if ( idxTarget == 0 )
+				{
+					printf( "%d", idxSrc );
+				}
+				else
+				{
+					printf( " " );
+				}
+				PrintStrSearch( srcStr, targetStr, idxSrc, idxSearchStart, idxTarget );
+			}
+
+			if ( srcStr[idxSearchStart] != targetStr[idxTarget] )
+			{
+				idxTarget = 0;
+				break;
+			}
+		}
+		if ( targetStr[idxTarget] == '\0' )
+		{
+			if ( isPrintProcess )
+			{
+				printf( "ºñ±³ È½¼ö: %d\n", count );
+			}
+
+			return idxSrc;
+		}
+	}
+	return idxSrc;
+}
+
+void PrintKMP( const char* srcStr, const char* targetStr, int srcCursor, int targetCursor, int subShiftCursor )
+{
+	printf( "%s\n", srcStr );
+	for ( int i = 0; i < srcCursor; ++i )
+	{
+		printf( " " );
+	}
+	if ( srcStr[srcCursor] == targetStr[targetCursor] )
+	{
+		printf( "+\n" );
+	}
+	else
+	{
+		printf( "|\n" );
+	}
+	for ( int i = 0; i < subShiftCursor; ++i )
+	{
+		printf( " " );
+	}
+	printf( "%s\n\n", targetStr );
+}
+
+void GetPattern( const char* subStr, int* patternArr, int size )
+{
+	for ( int i = 0, j = 1; j < size; )
+	{
+		if ( subStr[i] != subStr[j] )
+		{
+			if ( i == 0 )
+			{
+				j++;
+			}
+			else
+			{
+				i = patternArr[i];
+			}
+		}
+		else
+		{
+			patternArr[j] = ++i;
+			j++;
+		}
+	}
+}
+
+int FindSubStr_KMP( const char* srcStr, const char* subStr, int subStrSize, bool isPrintArr = false )
+{
+	int* patternArr = new int[subStrSize]();
+	GetPattern( subStr, patternArr, subStrSize );
+
+	if ( isPrintArr )
+	{
+		for ( int i = 0; i < subStrSize; ++i )
+		{
+			std::cout << patternArr[i] << " ";
+		}
+		std::cout << std::endl;
+	}
+	
+	int i = 0;
+	int j = 0;
+
+	if ( isPrintArr )
+	{
+		PrintKMP( srcStr, subStr, i, j, i - j );
+	}
+
+	for ( ; srcStr[i] != 0 && subStr[j] != 0; )
+	{
+		if ( srcStr[i] == subStr[j] )
+		{
+			++i;
+			++j;
+		}
+		else if ( j != 0 )
+		{
+			j = patternArr[j - 1];
+		}
+		else
+		{
+			++i;
+		}
+
+		if ( srcStr[i] == '\0' )
+		{
+			delete[] patternArr;
+			return i - j;
+		}
+
+		if ( isPrintArr )
+		{
+			PrintKMP( srcStr, subStr, i, j, i - j );
+		}
+	}
+
+	delete[] patternArr;
+	return -1;
+}
